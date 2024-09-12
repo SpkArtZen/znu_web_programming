30d6d73a92531ca84f43bbefb0db1e0c0bb7fcec
UserName <spkzeno@gmail.com>
2024-09-12 12:07:09 +0300

    lab5_webpack_fix

--- lab5/webpack.config.js
+++ lab5/webpack.config.js
@@ -15,28 +15,8 @@ module.exports = {
   },
   plugins: [
     new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      title: "Index",
-      template: path.resolve(__dirname, "./src/pages/index.html"),
-      filename: "index.html",
-    }),
-    new HtmlWebpackPlugin({
-      title: "Photo",
-      template: path.resolve(__dirname, "./src/pages/photo.html"),
-      filename: "photo.html",
-    }),
-    new HtmlWebpackPlugin({
-      title: "rozklad",
-      template: path.resolve(__dirname, "./src/pages/rozklad.html"),
-      filename: "rozklad.html",
-    }),
-    new HtmlWebpackPlugin({
-      title: "News",
-      template: path.resolve(__dirname, "./src/pages/news.html"),
-      filename: "news.html",
-    }),
     new CopyPlugin({
-      patterns: [{ from: "./src/assets", to: "assets" }],
+      patterns: [{ from: "./src", to: "src" }],
     }),
     new MiniCssExtractPlugin(),
   ],
